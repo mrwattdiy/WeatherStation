163955170665d3b0bea784acd2664a37a7e1d66c
mrwattdiy <info@mrwatt.eu>
2021-06-24 10:21:25 +0200

    Aggiunto nuovo progetto WeatherStationD1R2

---
+++ Wemos/WeatherStationWemosD1R2/secrets.h
@@ -0,0 +1,14 @@
+// Use this file to store all of the private credentials
+// and connection details
+
+#define SECRET_SSID "VodafoneSurfer"            // replace MySSID with your WiFi network name
+#define SECRET_PASS "B311a@P3r.Te"      // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID_WEATHER_STATION 634036                     //MathWorks weather station
+
+#define SECRET_CH_ID_COUNTER 298725                                     //Test channel for counting
+#define SECRET_READ_APIKEY "VGU0BO3N154VDGF2"   //API Key for Test channel
+#define SECRET_WRITE_APIKEY "2A9EW6XI1SWUAXJC"
+
+// ThingSpeak Certificate Fingerprint, Expiration Date: August 3, 2022 at 8:00:00 AM EST 
+#define SECRET_SHA1_FINGERPRINT "27 18 92 DD A4 26 C3 07 09 B9 7A E6 C5 21 B9 5B 48 F7 16 E1"
